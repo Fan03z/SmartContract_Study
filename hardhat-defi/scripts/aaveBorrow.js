8cbb98226f37b407d40bf322fb3ba7b2ef86aedc
Fan03z <2793791408@qq.com>
2023-02-23 14:17:14 +0800

    23.2.23

--- hardhat-defi/scripts/aaveBorrow.js
+++ hardhat-defi/scripts/aaveBorrow.js
@@ -30,6 +30,97 @@ async function main() {
   await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
 
   console.log("Deposited!");
+
+  // 通过质押价值进行借贷:
+
+  // 获得当前账户的借贷信息
+  let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+    lendingPool,
+    deployer
+  );
+
+  // 获得DAI/ETH换算汇率
+  const daiPrice = await getDaiPrice();
+  // 计算出最大借贷价值,并将单位换算为稳定币DAI
+  const amountDaiToBorrow =
+    availableBorrowsETH * 0.95 * (1 / daiPrice.toNumber());
+
+  console.log(`You can borrow ${amountDaiToBorrow} DAI`);
+
+  // 换算为Wei单位
+  const amountDaiToBorrowWei = ethers.utils.parseEther(
+    amountDaiToBorrow.toString()
+  );
+
+  // mainnet上的DaiAddress合约地址: 0x6B175474E89094C44Da98b954EedeAC495271d0F
+  const daiTokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+  await borrowDai(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer);
+
+  // 打印验证当前信息
+  await getBorrowUserData(lendingPool, deployer);
+
+  // 还借贷款:
+  await repay(amountDaiToBorrowWei, daiTokenAddress, lendingPool, deployer);
+
+  // 打印验证当前信息
+  await getBorrowUserData(lendingPool, deployer);
+}
+
+// 实现还借贷款功能
+async function repay(amount, daiAddress, lendingPool, account) {
+  // 授权借贷池从我们账户中取走借贷的代币
+  await approveErc20(daiAddress, lendingPool.address, amount, account);
+  // repay()具体用法看AAVE官方文档
+  const repayTx = await lendingPool.repay(daiAddress, amount, 1, account);
+  await repayTx.wait(1);
+
+  console.log("Repaid!");
+}
+
+// 实现借贷功能
+async function borrowDai(
+  daiAddress,
+  lendingPool,
+  amountDaiToBorrowWei,
+  account
+) {
+  // 调用lendingPool合约内的borrow()来借贷,具体用法可看官方文档
+  const borrowTx = await lendingPool.borrow(
+    daiAddress,
+    amountDaiToBorrowWei,
+    1,
+    0,
+    account
+  );
+  await borrowTx.wait(1);
+  console.log("You have borrowed!");
+}
+
+// 通过AggregatorV3Interface.sol价格计算,换算ETH为稳定币Dai计算价值
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface",
+    // AggregatorV3Interface中对应的代币换算地址到chainlink文件上查看
+    "0x773616E4d11A78F511299002da57A0a94577F1f4"
+    // 针对此合约不发起交易,可以不连接到部署账户
+  );
+  // AggregatorV3Interface合约中存在latestRoundData(),返回最新的数据 (可到合约源码查看具体返回数据)
+  // 其中价格换算汇率对应的下标为1
+  const price = (await daiEthPriceFeed.latestRoundData())[1];
+
+  console.log(`The DAI/ETH price is ${price.toString()}`);
+
+  return price;
+}
+
+// lendingPool合约内提供 getUserAccountData() ,可以查询到当前账户信息,包括抵押价值、可借贷价值等
+async function getBorrowUserData(lendingPool, account) {
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+    await lendingPool.getUserAccountData(account);
+  console.log(`价值 ${totalCollateralETH} ETH存储抵押`);
+  console.log(`已借贷价值 ${totalDebtETH} ETH`);
+  console.log(`可借贷价值 ${availableBorrowsETH} ETH`);
+  return { availableBorrowsETH, totalDebtETH };
 }
 
 // 实现得到借贷池方法
