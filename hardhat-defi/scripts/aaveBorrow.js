8302955b4508113c3040162798c80398ad483cc8
Fan03z <2793791408@qq.com>
2023-02-24 10:17:55 +0800

    23.2.23

--- hardhat-defi/scripts/aaveBorrow.js
+++ hardhat-defi/scripts/aaveBorrow.js
@@ -3,7 +3,7 @@ const { getWeth, AMOUNT } = require("../scripts/getWeth");
 const { getNamedAccounts, ethers } = require("hardhat");
 
 async function main() {
-  // AAVE平台的接待协议只支持 ERC20 协议的代币
+  // AAVE的接待协议只支持 ERC20 协议的代币
   // 而 ETH 不是ERC20协议的代币,要先转化为 WETH 代币才能进入AAVE平台成为抵押资产
   // 在getWeth.js上实现
   await getWeth();
