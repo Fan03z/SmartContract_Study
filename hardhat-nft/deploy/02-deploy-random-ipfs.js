d7b03967cda566e6a03133440e561fead5f65cb2
Fan03z <2793791408@qq.com>
2023-02-24 23:13:36 +0800

    23.2.24

---
+++ hardhat-nft/deploy/02-deploy-random-ipfs.js
@@ -0,0 +1,34 @@
+const { network, ethers } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+module.exports = async function (hre) {
+  const { getNamedAccounts, deployments } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  // 部署VRFCoordinatorV2,基本同Lottery项目上的部署步骤
+  let vrfCoordinatorV2Address, subscriptionId;
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      "VRFCoordinatorV2Mock"
+    );
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+    const tx = await vrfCoordinatorV2Mock.createSubscription();
+    const txReceipt = tx.wait(1);
+    subscriptionId = txReceipt.events[0].args.subId;
+  } else {
+    vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2;
+    subscriptionId = networkConfig[chainId].subscriptionId;
+  }
+
+  log("----------------------");
+
+  // 传入部署RandomIpfsNft.sol中的参数
+  const args = [];
+};
