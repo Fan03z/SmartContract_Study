fb42ae887996c792c62de6df5a956b65e3d6ba88
Fan03z <2793791408@qq.com>
2023-03-03 13:34:16 +0800

    23.3.3

--- NFTMarketplace/nextjs-nft-marketplace/pages/_app.js
+++ NFTMarketplace/nextjs-nft-marketplace/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+// 从react-moralis中导入MoralisProvider
+import { MoralisProvider } from "react-moralis";
+// 导入Headers
+import Header from "../components/Header";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // 不使用其服务器
+    <MoralisProvider initializeOnMount={false}>
+      <Header />
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
