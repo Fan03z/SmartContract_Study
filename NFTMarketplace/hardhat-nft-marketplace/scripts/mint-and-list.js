4514d28c5a253b37bfeca7d6b2c66d37f72ca545
Fan03z <2793791408@qq.com>
2023-03-04 18:14:13 +0800

    23.3.4

--- NFTMarketplace/hardhat-nft-marketplace/scripts/mint-and-list.js
+++ NFTMarketplace/hardhat-nft-marketplace/scripts/mint-and-list.js
@@ -1,4 +1,5 @@
-const { ethers } = require("hardhat");
+const { ethers, network } = require("hardhat");
+const { moveBlocks } = require("../utils/move-blocks");
 
 const PRICE = ethers.utils.parseEther("0.1");
 
@@ -23,6 +24,11 @@ async function mintAndList() {
   await tx.wait(1);
 
   console.log("NFT Listed!");
+
+  // 在测试网上手动控制出块
+  if (network.config.chainId === 31337) {
+    await moveBlocks(1, sleepAmont(1000));
+  }
 }
 
 mintAndList()
---
+++ NFTMarketplace/hardhat-nft-marketplace/utils/move-blocks.js
@@ -0,0 +1,26 @@
+// 手动控制区块出块
+const { network } = require("hardhat");
+
+function sleep(timeInMs) {
+  return new Promise((resolve) => setTimeout(resolve, timeInMs));
+}
+
+async function moveBlocks(amount, sleepAmount = 0) {
+  console.log("Moving blocks...");
+  for (let index = 0; index < amount; index++) {
+    await network.provider.request({
+      method: "evm_mine",
+      params: [],
+    });
+    if (sleepAmount) {
+      console.log(`Sleeping for ${sleepAmount}`);
+      await sleep(sleepAmount);
+    }
+  }
+  console.log(`Moved ${amount} blocks`);
+}
+
+module.exports = {
+  moveBlocks,
+  sleep,
+};
